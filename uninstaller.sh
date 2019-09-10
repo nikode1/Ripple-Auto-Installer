08f0cd3bacc55b958626a1389bb9b0516b32b730
Angel Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-09-10 16:33:32 +0600

    Update uninstaller.sh

--- uninstaller.sh
+++ uninstaller.sh
@@ -31,38 +31,30 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
 		libgle3 \
 		python-dev -y
     
-    apt-get purge git curl python3-pip python3-mysqldb -y
-	apt-get purge python-dev libmysqlclient-dev nginx software-properties-common libssl-dev mysql-server* mysql* -y
-  
-    apt-get purge php7.0* php7.0-mbstring php7.0-mcrypt php7.0-fpm php7.0-curl php7.0-mysql golang-go -y
-	
-	apt-get purge composer -y
-	apt-get purge zip unzip php7.0-zip -y
-  
-    apt-get purge mysql-client-core-5.7
-    apt-get purge mysql-server-core-5.7
-    
-    sudo rm -rf /var/log/mysql
-
-    sudo rm -rf /var/log/mysql.*
-
-    sudo rm -rf /var/lig/mysql
-
-    sudo rm -rf /var/etc/mysql
-    apt autoremove
-    rm -rf /var/lib/dpkg/lock
-	sudo du -sh /var/cache/apt
-	sudo apt-get clean
-
-	sudo apt-get update && sudo apt-get upgrade -y
-    sudo apt-get update
-	apt-get -qq update && apt-get -qq dist-upgrade
+apt-get purge git curl python3-pip python3-mysqldb -y
+apt-get purge python-dev libmysqlclient-dev nginx software-properties-common libssl-dev mysql-server* mysql* -y
+apt-get purge php7.0* php7.0-mbstring php7.0-mcrypt php7.0-fpm php7.0-curl php7.0-mysql golang-go -y
+apt-get purge composer -y
+apt-get purge zip unzip php7.0-zip -y
+apt-get purge mysql-client-core-5.7
+apt-get purge mysql-server-core-5.7
+sudo rm -rf /var/log/mysql
+sudo rm -rf /var/log/mysql.*
+sudo rm -rf /var/lig/mysql
+sudo rm -rf /var/etc/mysql
+apt autoremove
+rm -rf /var/lib/dpkg/lock
+sudo du -sh /var/cache/apt
+sudo apt-get clean
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update
+apt-get -qq update && apt-get -qq dist-upgrade
 else
     echo Done Uninstalling Dependencies
 fi
 
   
-  echo "package removed. Do you want to remove ripple source files?  (y/n) "
+echo "package removed. Do you want to remove ripple source files?  (y/n) "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
     echo Continuing
